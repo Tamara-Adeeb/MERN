b8821885041d8ddaf1018f45623e58f8da4ad390
Tamara Adeeb <tamara.adeeb1@gmail.com>
2021-08-02 11:36:18 +0300

    Fixing The First React With JSX

---
+++ React/First React With JSX/first/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello Dojo!
+        </h1>
+        <h2> Things i need to do:</h2>
+      <ol>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li> Run a marathon</li>
+        <li> Feed the dogs</li>
+      </ol>
+      </header>
+    </div>
+  );
+}
+
+export default App;
